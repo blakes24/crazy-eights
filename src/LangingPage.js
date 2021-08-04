a463956c0685d6c247be3cb2510b3af7f983d5e1
blakes24 <blakes24@gmail.com>
2021-08-03 17:14:09 -0700

    Add media queries to make design more responsive

--- src/LangingPage.js
+++ src/LangingPage.js
@@ -11,39 +11,61 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 function LandingPage() {
   return (
     <>
-      <h1 className={styles.title}>Crazy Eights</h1>
-      <div className={styles.main}>
-        <div className={styles.rules}>
-          <h2>How to Win</h2>
-          <p>Be the first to discard all of your cards to win the game.</p>
+      <div className={styles.title}>
+        <h1>Crazy Eights</h1>
+        <div className={styles.buttons}>
+          <a href="#about" className={styles.play}>
+            Learn More
+          </a>
           <Link to="/game" className={styles.play}>
-            Start Game
-          </Link>
-          <h2>How to Play</h2>
-          <p>
-            Click on a card from your hand that matches either the suit or the
-            value of the top card in the discard pile.{" "}
-            <i>
-              Example: If the top card is a six of clubs, you can play any card
-              that is a club or a six.
-            </i>
-          </p>
-          <img src={playCard} alt="play a card" className={styles.gif} />
-          <p>
-            If you don't have any cards that are playable you must click on the
-            deck to draw a card. After drawing a card you can play a card, draw
-            another card, or pass.
-          </p>
-          <img src={drawCard} alt="draw a card" className={styles.gif} />
-          <p>
-            Eights are wild and can be played at any time. When you play an
-            eight you will choose what suit must be played next.
-          </p>
-          <img src={playEight} alt="play an eight" className={styles.gif} />
-          <Link to="/game" className={styles.play}>
-            Start Game
+            Play Now
           </Link>
         </div>
+      </div>
+      <div className={styles.main} id="about">
+        <div className={styles.rules}>
+          <h2>How to Play</h2>
+          <p>Be the first to discard all of your cards to win the game.</p>
+          <div className={styles.row}>
+            <div className={styles.rowText}>
+              <h3>Play a card</h3>
+              <p>
+                Click on a card from your hand that matches either the suit or
+                the value of the top card in the discard pile.{" "}
+                <i>
+                  Example: If the top card is a six of clubs, you can play any
+                  card that is a club or a six.
+                </i>
+              </p>
+            </div>
+            <img src={playCard} alt="play a card" className={styles.gif} />
+          </div>
+          <div className={`${styles.row} ${styles.reverse}`}>
+            <div className={styles.rowText}>
+              <h3>Draw a card</h3>
+              <p>
+                If you don't have any cards that are playable you must click on
+                the deck to draw a card. After drawing a card you can play a
+                card, draw another card, or pass.
+              </p>
+            </div>
+            <img src={drawCard} alt="draw a card" className={styles.gif} />
+          </div>
+          <div className={styles.row}>
+            <div className={styles.rowText}>
+              <h3>Eights are wild</h3>
+              <p>
+                Eights can be played at any time. When you play an eight you get
+                to choose what suit must be played next.
+              </p>
+            </div>
+
+            <img src={playEight} alt="play an eight" className={styles.gif} />
+          </div>
+        </div>
+        <Link to="/game" className={styles.play}>
+          Start Game
+        </Link>
       </div>
       <footer className={styles.footer}>
         Blakely Burns
